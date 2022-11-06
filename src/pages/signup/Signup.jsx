b717eaf692883bc7a22da36b46fc50d2e6258f98
Haseeb <qureshihaxeeb2@gmail.com>
2022-11-07 01:12:42 +0530

    basic additions of functions!

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,29 +1,70 @@
 import { Done, Error } from '@mui/icons-material'
-import { Box, Button, CircularProgress, Grid, InputAdornment, Stack, TextField, Typography } from '@mui/material'
+import { Alert, Box, Button, CircularProgress, Grid, InputAdornment, Snackbar, Stack, TextField, Typography } from '@mui/material'
 import { AnimatePresence } from 'framer-motion'
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { Fade } from '../../components/AnimationEngine'
 
 export default function Signup() {
 
-    const [isDisabled, setIsDisabled] = useState(false)
+    const [isDisabled, setIsDisabled] = useState(true)
     const [loading, setLoading] = useState(false)
     const [isUnique, setIsUnique] = useState(false)
+
+    const staticUserRef = '123'
+
+    const [openSnack, setOpenSnack] = useState(false)
+    const [snackText, setSnackText] = useState(false)
+
+    const usernameRef = useRef()
+    const emailRef = useRef()
+    const passwordRef1 = useRef()
+    const passwordRef2 = useRef()
 
     const navigate = useNavigate()
     const location = useLocation()
 
-    function HandleSubmit(e) {
+    async function HandleSubmit(e) {
         e.preventDefault()
-        setIsDisabled(!isDisabled)
-        setLoading(true)
-        console.log('submitted!')
+        const pass1 = passwordRef1.current.value
+        const pass2 = passwordRef2.current.value
+        if (pass1 === pass2) {
+            setIsDisabled(!isDisabled)
+            setLoading(true)
+            console.log(pass1, pass2)
+            console.log('submitted!')
+        }
+
+        else {
+            setOpenSnack(!openSnack)
+            setSnackText("Passwords don't match")
+        }
+    }
+
+    function CheckUsername(event) {
+        if (event.target.value === staticUserRef) {
+            setIsDisabled(false)
+            setIsUnique(true)
+        }
+        else {
+            setOpenSnack(!openSnack)
+            setIsUnique(false)
+            setIsDisabled(true)
+            setSnackText("Please choose a different username")
+        }
     }
 
     return (
         <Fade key={location.pathname}>
             <Box sx={{ display: 'flex', flexDirection: 'column', width: { xs: '90%', md: '90%', lg: '100%' }, m: 'auto', marginTop: '5vh', height: "95vh", justifyContent: "center", alignItems: "center" }} gap={5}>
+
+                <Snackbar
+                    open={openSnack}
+                    anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+                    autoHideDuration={6000}
+                    onClose={() => setOpenSnack(!openSnack)}>
+                    <Alert severity="error" variant='filled'>{snackText}</Alert>
+                </Snackbar>
 
                 <Stack sx={{ textAlign: 'center' }}>
                     <Typography sx={{ fontWeight: 500 }} component='span' variant='h2'>HIAM</Typography>
@@ -33,20 +74,20 @@ export default function Signup() {
                 <Box sx={{ width: { sm: '85%', lg: '30%' } }} >
                     <form onSubmit={(e) => HandleSubmit(e)}>
                         <Stack gap={5} direction={'column'}>
-                            <TextField label="Username" variant="standard" placeholder='Select a unique username' required InputProps={{
+                            <TextField onChange={CheckUsername} inputRef={usernameRef} label="Username" variant="standard" placeholder='Select a unique username' required InputProps={{
                                 endAdornment: (
                                     <InputAdornment position="start">
                                         {isUnique == true ? <Done sx={{ color: 'green' }} /> : <Error sx={{ color: 'red' }} />}
                                     </InputAdornment>
                                 ),
                             }} />
-                            <TextField label="email" variant="standard" type="email" required />
+                            <TextField inputRef={emailRef} label="email" variant="standard" type="email" required />
                             <Grid container spacing={2}>
                                 <Grid item xs={6}>
-                                    <TextField label="Password" type="password" variant="standard" required />
+                                    <TextField inputRef={passwordRef1} label="Password" type="password" variant="standard" required />
                                 </Grid>
                                 <Grid item xs={6}>
-                                    <TextField label="Confirm Password" type="password" variant="standard" required />
+                                    <TextField inputRef={passwordRef2} label="Confirm Password" type="password" variant="standard" required />
                                 </Grid>
                             </Grid>
                             <Button disabled={isDisabled} type="submit" sx={{ padding: '5px 90px' }} variant="contained" >SIGNUP</Button>
